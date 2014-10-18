a52d869110af5529b4b2106d631639b98116e99c
wenright <wenright@purdue.edu>
2014-10-18 15:02:06 -0400

    A few changes. Stashing to try to fix site.

--- TimesWords/Parser.py
+++ TimesWords/Parser.py
@@ -1,4 +1,26 @@
 __author__ = 'will'
 
+import urllib
+from HTMLParser import HTMLParser
+
 #Parser class
 class Parser:
+    @staticmethod
+    def downloadURL(urlToLoad):
+        url = urllib.urlopen(urlToLoad)
+        html = url.read()
+        print (html)
+        return html
+
+
+    def main(self):
+        parser = ParseHTML()
+        parser.feed(Parser.downloadURL("https://www.google.com"))
+        parser.close()
+
+class ParseHTML(HTMLParser):
+    def hand_starttag(selfself, tag, attrs):
+        print(tag)
+
+
+Parser().main();
