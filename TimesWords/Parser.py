7d79c59613365a18716882f12aa1e634d8e6f80f
wenright <wenright@purdue.edu>
2014-10-18 15:23:24 -0400

    Added Parser class

--- TimesWords/Parser.py
+++ TimesWords/Parser.py
@@ -1,25 +1,27 @@
 __author__ = 'will'
 
-import urllib
+import urllib2
 from HTMLParser import HTMLParser
+from cookielib import CookieJar
 
 #Parser class
 class Parser:
     @staticmethod
     def downloadURL(urlToLoad):
-        url = urllib.urlopen(urlToLoad)
-        html = url.read()
-        print (html)
+        cookieJar = CookieJar()
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookieJar))
+        p = opener.open(urlToLoad)
+        html = p.read()
         return html
 
 
     def main(self):
         parser = ParseHTML()
-        parser.feed(Parser.downloadURL("https://www.google.com"))
+        parser.feed(Parser.downloadURL("http://www.nytimes.com/2014/10/18/world/unbowed-putin-chews-the-scenery-in-milan.html?ref=world"))
         parser.close()
 
 class ParseHTML(HTMLParser):
-    def hand_starttag(selfself, tag, attrs):
+    def hand_starttag(self, tag, attrs):
         print(tag)
 
 
