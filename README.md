5968ee72cda320c502f2386561c59f493a37bfef
Grayson Ricketts <graysonricketts@pal-nat184-211-196.itap.purdue.edu>
2014-10-18 09:23:15 -0400

    API Query now has a daily query call

--- README.md
+++ README.md
@@ -4,3 +4,15 @@ Boilermake Fall 2014 Project.
 
 Webpages for wordsthroughti.me
 
+TODO::::
+	GRAYSON:
+		API calls/ parser
+		Word frequency
+		General helper
+	WILL H:
+		Word frequency and formatting
+	WILL E:
+		Word frequency and formatting
+	TANNER:
+		front end/ website
+		javascript creation of wordmaps
--- TimesWords/APIConnector.py
+++ TimesWords/APIConnector.py
@@ -2,24 +2,41 @@ __author__ = 'graysonricketts'
 
 import urllib.request
 import json
+import time
+import datetime
+import string
 
 #Connector class
 class Connector:
     key = 'abeec3ec164c8158e4250bd657bd36d9:15:69090296'
 
-    @staticmethod
-    def JSONQuery():
-        startDate = '20130701'
-        endDate = '20130726'
+    #Takes in a start date, end date, and a page
+    #Gets 10 URLs from the api, sorted so that the newest are opened first
+    def JSONGeneralQuery(startDate, endDate, page):
+        #'20130701'
+        #'20130726'
 
-        query = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?&fq=new+york+times&fl=web_url&fq=news_desk:("Foreign"+"National")&begin_date=' + startDate + '&end_date=' + endDate + '&api-key=' + Connector.key
+        query = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?&fq=new+york+times&fl=web_url&fq=news_desk:("Foreign"+"National")&begin_date=' + startDate + '&end_date=' + endDate + '&sort=newest&page=' + page + '&api-key=' + Connector.key
 
         page = urllib.request.urlopen(query)
         urls = json.loads(page.read().decode('utf-8'))
 
         return urls['response']['docs']
 
-x = Connector.JSONQuery()
+    def JSONDailyQuery():
+        today = str(datetime.date.today().year) + str(datetime.date.today().month) + str(datetime.date.today().day)
+        list = []
+        x = 0
 
-while x:
-    print(x.pop())
+        while(Connector.JSONGeneralQuery(today, today, str(x))):
+            temp = Connector.JSONGeneralQuery(today, today, str(x))
+            while(temp):
+                list.append(temp.pop())
+            x += 1
+            time.sleep(.5)
+
+        print(list)
+        return list
+
+c = Connector
+print(c.JSONDailyQuery())
