f6deef8792cbeff6c080a09c169f8787228efa6a
whuang8 <william_huang8@yahoo.com>
2014-10-18 15:18:41 -0400

    Remove CNAME from master

--- TimesWords/Parser.py
+++ TimesWords/Parser.py
@@ -1,4 +1,12 @@
 __author__ = 'will'
 
-#Parser class
-class Parser:
+
+import urllib2
+
+req = urllib2.Request('http://www.nytimes.com/2014/10/18/opinion/arthur-c-brooks-start-helping-the-helpers.html?hp&action=click&pgtype=Homepage&module=c-column-top-span-region&region=c-column-top-span-region&WT.nav=c-column-top-span-region')
+response = urllib2.urlopen(req)
+the_page = response.read()
+
+
+		
+
