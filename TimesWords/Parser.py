b00e6acac46f6aca6c8d0fdb290e2dee070befd0
wenright <wenright@purdue.edu>
2014-10-18 21:05:45 -0400

    Detects common words and removes them

--- TimesWords/Parser.py
+++ TimesWords/Parser.py
@@ -23,19 +23,31 @@ class Parser:
         return dom.find("p.story-body-text").text()
 
     @staticmethod
+    def __isLegalWord__(word):
+        disallowedWords = ["A", "AN", "FOR", "TO", "I", "WE", "US", "THE", "AND", "OF", "THEIR", "HE", "THEM", "IN"]
+        for check in disallowedWords:
+            if (word == check):
+                return False
+
+        return True
+
+    @staticmethod
     def __stringToArray__(article):
         string_array = []
         last_space = 0
         count = 0
         for c in article:
             if (c == ' '):
-                string_array.append(re.sub(r'\W+','', string.upper(article[last_space:count])))
+                wordToAdd = re.sub(r'\W+','', string.upper(article[last_space:count]))
+                if (wordToAdd.__len__() > 0 and Parser.__isLegalWord__(wordToAdd)):
+                    string_array.append(wordToAdd)
                 last_space = count
             count = count + 1
-
 
         return string_array
     
     @staticmethod
     def getWordArrayFromURL(URL):
-        return Parser.stringToArray(Parser.parseURL(URL))
+        return Parser.__stringToArray__(Parser.__parseURL__(URL))
+
+print(Parser.getWordArrayFromURL("http://www.nytimes.com/2014/10/19/world/americas/missing-mexican-student-search.html?ref=world"))
