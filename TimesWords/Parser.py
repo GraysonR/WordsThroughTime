0f95ed441f51cb847803b57b7b83b925d70e357b
wenright <wenright@purdue.edu>
2014-10-18 23:09:11 -0400

    Newline thing again....

--- TimesWords/Parser.py
+++ TimesWords/Parser.py
@@ -23,15 +23,6 @@ class Parser:
         return dom.find("p.story-body-text").text()
 
     @staticmethod
-    def __isLegalWord__(word):
-        disallowedWords = ["A", "AN", "FOR", "TO", "I", "WE", "US", "THE", "AND", "OF", "THEIR", "HE", "THEM", "IN"]
-        for check in disallowedWords:
-            if (word == check):
-                return False
-
-        return True
-
-    @staticmethod
     def __stringToArray__(article):
         string_array = []
         last_space = 0
@@ -39,13 +30,12 @@ class Parser:
         for c in article:
             if (c == ' ' or c == '\n'):
                 wordToAdd = re.sub(r'\W+','', string.upper(article[last_space:count]))
-                if (wordToAdd.__len__() > 0 and Parser.__isLegalWord__(wordToAdd)):
+                if (wordToAdd.__len__() > 0):
                     string_array.append(wordToAdd)
-                last_space = count
+                    last_space = count
             count = count + 1
-
         return string_array
-    
+
     @staticmethod
     def getWordArrayFromURL(URL):
         return Parser.__stringToArray__(Parser.__parseURL__(URL))
